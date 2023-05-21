e00bbbe678c967f1e08c5004656c681c57334421
Fahad <daylight1498@gmail.com>
2023-05-21 18:04:24 +0530

    Added hosting mongod server URL

--- back-end/config/database.js
+++ back-end/config/database.js
@@ -4,7 +4,10 @@ mongoose.set("strictQuery", false);
 
 const configureDB = () => {
   mongoose
-    .connect("mongodb://localhost:27017/expense-app")
+    // .connect("mongodb://localhost:27017/expense-app")
+    .connect(
+      "mongodb+srv://daylight1498:a9kl0C97SbSFfxG6@cluster0.b2dlfga.mongodb.net/"
+    )
     .then(() => {
       console.log("connected to Db");
     })
